9178b3b88dc64026cf3ae94ed8043934ac3f76be
Angel Penkov <angel.penkov@bbraun.com>
2019-08-24 17:50:49 +0300

    Test2

---
+++ src/Json.java
@@ -0,0 +1,11 @@
+public class Json {
+
+    String json = "{\"a\": 23\n" +
+            "\n" +
+            "\"zzzzz\":  \"wwwwwwwwww\"";
+
+    public static void main(String[] args) {
+        System.out.println("qqqqqqqqqqqqqqqqq");
+    }
+
+}
---
+++ src/pack1/Test2.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import org.junit.Test;
+
+public class Test2 {
+    @Test
+    public void test1(){
+        System.out.println("Test2 - test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 - test2");
+    }
+}
