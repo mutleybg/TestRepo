1f4d421e915396678e4ce53f529bc8756c788e1c
Angel Penkov <angel.penkov@bbraun.com>
2019-08-25 16:12:35 +0300

    One more test file

---
+++ src/pack1/Test3.java
@@ -0,0 +1,15 @@
+package pack1;
+
+import org.junit.Test;
+
+public class Test3 {
+    @Test
+    public void test1(){
+        System.out.println("Test1 - test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test1 - test2");
+    }
+}
